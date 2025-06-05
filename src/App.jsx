1c3a0871bf325d2d7f7d033b75eb31268377a9fa
sejanmiah <sejankhan931@gmail.com>
2025-06-05 14:31:10 +0400

    Layout Ready

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import HeroSection from './HeroSection';
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Header></Header>
+      <HeroSection></HeroSection>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default App;
